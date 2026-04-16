1389b53270821192f1e444ac532427ff9cad6012
VarunMeda <varun.m.l2006@gmail.com>
2026-04-16 19:16:18 +0000

    Phase 3: implemented index_add

--- index.c
+++ index.c
@@ -218,8 +218,40 @@ int index_save(const Index *index) {
 //
 // Returns 0 on success, -1 on error.
 int index_add(Index *index, const char *path) {
-    // TODO: Implement file staging
-    // (See Lab Appendix for logical steps)
-    (void)index; (void)path;
-    return -1;
+
+    FILE *f = fopen(path, "rb");
+    if (!f) return -1;
+
+    fseek(f, 0, SEEK_END);
+    size_t size = ftell(f);
+    rewind(f);
+
+    void *data = malloc(size);
+    fread(data, 1, size, f);
+    fclose(f);
+
+    ObjectID id;
+
+    // store blob (Phase 1)
+    if (object_write(OBJ_BLOB, data, size, &id) != 0) {
+        free(data);
+        return -1;
+    }
+
+    free(data);
+
+    struct stat st;
+    stat(path, &st);
+
+    // update index
+    IndexEntry *e = &index->entries[index->count++];
+
+    e->mode = get_file_mode(path);
+    e->hash = id;
+    e->mtime_sec = st.st_mtime;
+    e->size = size;
+
+    strcpy(e->path, path);
+
+    return 0;
 }
